ef22dddba9741a76ade5bb51657d87e00a9c3743
Nico <nico_cartellone@hotmail.com>
2023-04-15 17:45:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import './App.css'
+import { useMovies } from './hooks/useMovies'
+import { Movies } from './components/Movies'
+import { useState, useEffect, useRef, useCallback } from 'react'
+import debounce from 'just-debounce-it'
+
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
+
+function App () {
+  const [sort, setSort] = useState(false)
+
+  const { search, updateSearch, error } = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      console.log('search', search)
+      getMovies({ search })
+    }, 300)
+    , [getMovies]
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ search })
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debouncedGetMovies(newSearch)
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
+  return (
+    <div className="page">
+      <h1>Busca tus películas</h1>
+      <header>
+        <form className="form" onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={search} name='query' placeholder="Avengers, Start Wars, The Matrix..." />
+          <input type='checkbox' onChange={handleSort} checked={sort}/>
+          <button type="submit">Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies}/>
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
